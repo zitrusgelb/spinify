0082094c16f06433df61c65714111fcc20840a9d
Tim <tim.knuettel@study.thws.de>
2025-06-11 09:40:44 +0200

    style: remove semicolons

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -1,11 +1,11 @@
 // https://vike.dev/Head
 
-import logoUrl from "../assets/logo.png";
+import logoUrl from "../assets/logo.png"
 
 export default function HeadDefault() {
   return (
     <>
       <link rel="icon" href={logoUrl} />
     </>
-  );
+  )
 }
