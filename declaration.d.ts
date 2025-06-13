9e96b6ba993ea5d02545f2c3471d1f806ba23df2
Tim <tim.knuettel@study.thws.de>
2025-06-13 09:18:55 +0200

    fix: remove warning from TypeScript for wrong import of logos/icons

---
+++ declaration.d.ts
@@ -0,0 +1,11 @@
+declare module "*.png" {
+  const value: string
+  export default value
+}
+
+declare module "*.svg" {
+  import * as React from "react"
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+  const src: string
+  export default src
+}
