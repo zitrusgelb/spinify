54a247a99cdd0f2758f8e9cdb20cfbe5a3c438f4
Tim <tim.knuettel@study.thws.de>
2025-06-15 16:31:02 +0200

    refactor: remove unused import form vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,9 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import vike from "vike/plugin"
-import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
-  plugins: [vike(), react({}), tailwindcss(), svgr()],
+  plugins: [vike(), react({}), tailwindcss()],
   build: {
     target: "es2022",
   },
