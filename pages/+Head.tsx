2f36ddf1be0c5a130ddf4a2d1449895c292a04a7
Tim <tim.knuettel@study.thws.de>
2025-06-15 16:29:11 +0200

    docs: remove unnecessary ignore annotation

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -1,7 +1,5 @@
 // https://vike.dev/Head
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
 import logoUrl from "../assets/logo.png"
 
 export default function HeadDefault() {
