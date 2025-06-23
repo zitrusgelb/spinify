3562117925091b422f2794a204cf0d4bdd2205b5
Tim <tim.knuettel@study.thws.de>
2025-06-23 11:56:49 +0200

    feat: basic components for user-insights

--- pages/userInsights/+Page.tsx
+++ pages/userInsights/+Page.tsx
@@ -1,7 +1,17 @@
 export default function Page() {
   return (
-    <>
-      <h1>User Insights</h1>
-    </>
+    <div className="flex flex-col gap-11 p-5  w-fit">
+      <MainElement title="User Insights" />
+      <MainElement title="Top Tracks" />
+      <MainElement title="Top Artists" />
+    </div>
+  )
+}
+
+function MainElement({ title }: { title: string }) {
+  return (
+    <div className=" font-bold text-3xl text-secondary bg-primary rounded-3xl p-5 w-fit">
+      <h2> {title}</h2>
+    </div>
   )
 }
