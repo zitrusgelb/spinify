fb57b705e9d3dd18c3f48ab423d0d1c386c98508
Tim <tim.knuettel@study.thws.de>
2025-06-23 12:13:24 +0200

    feat: first draft of buttons for user-insights

--- pages/userInsights/+Page.tsx
+++ pages/userInsights/+Page.tsx
@@ -10,8 +10,23 @@ export default function Page() {
 
 function MainElement({ title }: { title: string }) {
   return (
-    <div className=" font-bold text-3xl text-secondary bg-primary rounded-3xl p-5 w-fit">
-      <h2> {title}</h2>
+    <div className=" flex felx-row p-5 gap-5">
+      <div className=" font-bold text-3xl text-secondary bg-primary rounded-3xl w-fit h-fit p-5"> {title} </div>
+      <Buttons />
+    </div>
+  )
+}
+
+function Buttons() {
+  const buttons = [{ label: "Last 30 Days" }, { label: "Last 6 Months" }, { label: "Last Year" }]
+
+  return (
+    <div className="flex flex-row gap-5 w-fit h-fit">
+      {buttons.map((button, index) => (
+        <button key={index} className="btn bg-accent rounded-3xl w-fit h-fit text-background p-3 ">
+          {button.label}
+        </button>
+      ))}
     </div>
   )
 }
