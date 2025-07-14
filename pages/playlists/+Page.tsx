5b970bb35b7dbb342eed5f15c93c9f5fae507a29
Laurin <laurin@neubert.dev>
2025-07-14 16:42:07 +0200

    fix(playlists): fixed typo

--- pages/playlists/+Page.tsx
+++ pages/playlists/+Page.tsx
@@ -43,7 +43,7 @@ export default function Page() {
 
   function MainElement({ title }: { title: string }) {
     return (
-      <div className=" flex felx-row p-5 gap-5">
+      <div className=" flex flex-row p-5 gap-5">
         <div className=" font-bold text-3xl text-secondary bg-primary rounded-3xl w-fit h-fit p-5"> {title}</div>
       </div>
     )
