d5ed9d9be3f28003fed2790df00e1f38fd98768b
Laurin <laurin@neubert.dev>
2025-07-15 10:28:31 +0200

    feat(playlist overview): added saved playlists above user's playlist

--- pages/playlists/+Page.tsx
+++ pages/playlists/+Page.tsx
@@ -4,24 +4,53 @@ import ApiContext from "components/ApiContext"
 type Playlist = { title: string; thumbnail: string | null; id: string }
 
 export default function Page() {
+  const [userPlaylists, setUserPlaylists] = useState<Playlist[]>([])
   const [savedPlaylists, setSavedPlaylists] = useState<Playlist[]>([])
-  const { api, login } = useContext(ApiContext)
+  const { api, login, user } = useContext(ApiContext)
   const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-    login().then(() => fetchSavedPlaylists().finally(() => setLoading(false)))
+    login().then(() => console.log("Login successful"))
   }, [])
 
+  useEffect(() => {
+    if (user) {
+      fetchSavedPlaylists()
+        .then(fetchUserPlaylists())
+        .finally(() => setLoading(false))
+    }
+  }, [user])
+
   const fetchSavedPlaylists = async () => {
-    const res = await api.currentUser.playlists.playlists(30, 0)
+    const res = await fetchPlaylists()
     const results = await Promise.all(
-      res.items.map(async (playlist) => ({
-        id: playlist.id,
-        title: playlist.name,
-        thumbnail: playlist.images[0].url,
-      })),
+      res.items
+        .filter((playlist) => playlist.owner.id != user?.id)
+        .map(async (playlist) => ({
+          id: playlist.id,
+          title: playlist.name,
+          thumbnail: playlist.images[0].url,
+        })),
     )
     setSavedPlaylists(results)
+  }
+
+  const fetchUserPlaylists = async () => {
+    const res = await fetchPlaylists()
+    const results = await Promise.all(
+      res.items
+        .filter((playlist) => playlist.owner.id == user?.id)
+        .map(async (playlist) => ({
+          id: playlist.id,
+          title: playlist.name,
+          thumbnail: playlist.images[0].url,
+        })),
+    )
+    setUserPlaylists(results)
+  }
+
+  async function fetchPlaylists() {
+    return await api.currentUser.playlists.playlists(50, 0)
   }
 
   const handleOpen = (id: string) => {
@@ -29,7 +58,7 @@ export default function Page() {
   }
 
   return (
-    <div className="flex flex-col gap-11 p-5 w-fit">
+    <div className="flex flex-col">
       <MainElement title="Saved Playlists" />
       {loading ? (
         <div className="flex justify-center items-center h-64">
@@ -38,12 +67,20 @@ export default function Page() {
       ) : (
         <PlaylistGrid playlists={savedPlaylists} />
       )}
+      <MainElement title="Your Playlists" />
+      {loading ? (
+        <div className="flex justify-center items-center h-64">
+          <span className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary"></span>
+        </div>
+      ) : (
+        <PlaylistGrid playlists={userPlaylists} />
+      )}
     </div>
   )
 
   function MainElement({ title }: { title: string }) {
     return (
-      <div className=" flex flex-row p-5 gap-5">
+      <div className=" flex flex-row p-5 pb-0 gap-5">
         <div className=" font-bold text-3xl text-secondary bg-primary rounded-3xl w-fit h-fit p-5"> {title}</div>
       </div>
     )
@@ -51,7 +88,7 @@ export default function Page() {
 
   function PlaylistGrid({ playlists }: { playlists: Playlist[] }) {
     return (
-      <div className="flex gap-5 flex-cols-4 flex-wrap">
+      <div className="flex gap-5 max-w-full overflow-x-scroll scrollbar-styled">
         {playlists.map((playlist) => (
           <button
             key={playlist.id}
@@ -68,7 +105,7 @@ export default function Page() {
   function PlaylistElement({ thumbnail, title }: { thumbnail: string | null; title: string }) {
     return (
       <div className="flex content-center flex-col gap-5 p-5">
-        <div className="text-2xl text-center w-64 pl-4 pr-4 overflow-hidden overflow-ellipsis whitespace-nowrap">
+        <div className="text-xl font-bold text-center w-64 pl-4 pr-4 overflow-hidden overflow-ellipsis whitespace-nowrap">
           {title}
         </div>
         <img
