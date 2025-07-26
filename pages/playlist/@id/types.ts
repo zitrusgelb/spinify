639a1e00343f27cbd296e713746a9aa0c86ad1d9
Tim <tim.knuettel@study.thws.de>
2025-07-26 21:04:22 +0200

    refactor: remove image from type artist

--- pages/playlist/@id/types.ts
+++ pages/playlist/@id/types.ts
@@ -6,6 +6,6 @@ export type Track = {
   thumbnail: string
   durationMs: number
 }
-export type Artist = { id: string; name: string; image: string}
+export type Artist = { id: string; name: string;}
 
 export type Playlist = { title: string; thumbnail: string | null; id: string }
