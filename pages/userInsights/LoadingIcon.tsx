63ec65e7afb8ca9ddac8a585f8d31c948e7818dd
Tim <tim.knuettel@study.thws.de>
2025-07-28 13:01:27 +0200

    refactor: delete LoadingIcon and ImageRowElement

--- pages/userInsights/LoadingIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderCircleIcon } from "lucide-react"
-
-export default function LoadingIcon() {
-  return (
-    <div className="flex justify-center items-center h-64">
-      <LoaderCircleIcon className="animate-spin rounded-full h-12 w-12 border-b-2 border-primary" />
-    </div>
-  )
-}
