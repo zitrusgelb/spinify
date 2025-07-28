940e5893a1f8ac1367a4d68af63d572a713b4116
Tim <tim.knuettel@study.thws.de>
2025-07-28 16:36:33 +0200

    refactor: remove unnecessary sizing
    Signed-off-by: Tim <tim.knuettel@study.thws.de>

--- components/MainElementWithButtons.tsx
+++ components/MainElementWithButtons.tsx
@@ -9,7 +9,7 @@ interface MainElementWithButtonsProps {
 
 export default function MainElementWithButtons({ title, selectedRange, onRangeChange }: MainElementWithButtonsProps) {
   return (
-    <div className="overflow-x-auto -mx-5 px-5 scrollbar-styled">
+    <div className="overflow-x-auto scrollbar-styled">
       <div className=" flex flex-row p-5 gap-5 whitespace-nowrap flex-nowrap">
         <div className=" font-bold text-3xl text-secondary bg-primary rounded-3xl w-fit h-fit p-5">{title}</div>
         <Buttons selected={selectedRange} onSelect={onRangeChange} />
