bef49c38452ea67a93f58688f10cc6ab5f1799ba
Jakob <@k65990>
2025-07-29 11:15:08 +0200

    add: removed comment

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -44,7 +44,6 @@ export default function Page() {
 
   return (
     <div>
-      {/* Optional: testbutton */}
       <button
         onClick={() => setIsModalOpen(true)}
         className="mt-4 bg-[var(--color-primary)] text-white px-4 py-2 rounded"
