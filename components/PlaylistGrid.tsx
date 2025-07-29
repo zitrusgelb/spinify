2a8736b39aa8a087a448f2e170efc6ab9d2d1f76
Adrian Stelter <gitmac@zitrusgelb.com>
2025-07-29 12:53:29 +0200

    fix: no fallback image

--- components/PlaylistGrid.tsx
+++ components/PlaylistGrid.tsx
@@ -1,12 +1,16 @@
-import { SimplifiedPlaylist } from "@spotify/web-api-ts-sdk"
-import { Grid } from "components/Grid"
+import { SimplifiedPlaylist } from '@spotify/web-api-ts-sdk'
+import { Grid } from 'components/Grid'
 
 export default function PlaylistGrid({ playlists }: { playlists: SimplifiedPlaylist[] }) {
   return (
     <Grid>
-      {playlists.map((playlist) => (
+      {playlists.map(playlist => (
         <a href={`/playlist/${playlist.id}`} key={playlist.id}>
-          <PlaylistElement key={playlist.name} thumbnail={playlist.images[0].url} title={playlist.name} />
+          <PlaylistElement
+            key={playlist.name}
+            thumbnail={playlist.images[0]?.url ?? ''}
+            title={playlist.name}
+          />
         </a>
       ))}
     </Grid>
@@ -17,7 +21,7 @@ function PlaylistElement({ thumbnail, title }: { thumbnail: string | null; title
   return (
     <div className="flex flex-col justify-center items-center gap-5 p-5 h-full hover:scale-105 transition duration-200">
       <img
-        src={thumbnail ?? ""}
+        src={thumbnail ?? ''}
         alt={title}
         className="max-w-45 max-h-45 min-h-32 min-w-32 rounded-lg aspect-square object-cover"
       />
--- components/TrackGrid.tsx
+++ components/TrackGrid.tsx
@@ -1,12 +1,12 @@
-import { Track } from "@spotify/web-api-ts-sdk"
-import { Grid } from "components/Grid"
+import { Track } from '@spotify/web-api-ts-sdk'
+import { Grid } from 'components/Grid'
 
 export default function TrackGrid({ tracks }: { tracks: Track[] }) {
   return (
     <Grid>
-      {tracks.map((track) => (
+      {tracks.map(track => (
         <a href={`/album/${track.album.id}`} key={track.id}>
-          <TrackElement thumbnail={track.album.images[0].url} title={track.name} />
+          <TrackElement thumbnail={track.album.images[0]?.url ?? ''} title={track.name} />
         </a>
       ))}
     </Grid>
@@ -17,11 +17,13 @@ function TrackElement({ thumbnail, title }: { thumbnail: string; title: string }
   return (
     <div className="flex flex-col justify-center items-center gap-5 p-5 h-full hover:scale-105 transition duration-200">
       <img
-        src={thumbnail ?? ""}
+        src={thumbnail ?? ''}
         alt={title}
         className="max-w-45 max-h-45 min-h-32 min-w-32 rounded-lg aspect-square object-cover"
       />
-      <div className="font-bold text-center w-45 pl-4 pr-4 truncate whitespace-nowrap">{title ?? ""}</div>
+      <div className="font-bold text-center w-45 pl-4 pr-4 truncate whitespace-nowrap">
+        {title ?? ''}
+      </div>
     </div>
   )
 }
