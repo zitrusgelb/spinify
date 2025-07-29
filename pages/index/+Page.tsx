389c8066d6b725b28626747264a5a5184eab7602
Tim <tim.knuettel@study.thws.de>
2025-07-29 13:50:17 +0200

    refactor: remove button to open Fullscreen player

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -7,12 +7,9 @@ import Spinner from 'components/LoadingSpinner'
 import AlbumGrid from 'components/AlbumGrid'
 import { Artist, SimplifiedAlbum, Track } from '@spotify/web-api-ts-sdk'
 import TrackGrid from 'components/TrackGrid'
-import FullScreenPlayer from 'layouts/FullScreenPlayer'
-import MiniPlayer from 'layouts/MiniPlayer'
 
 export default function Page() {
   const { api, user } = useContext(ApiContext)
-  const [isModalOpen, setIsModalOpen] = useState(false)
 
   const [topSongs, setTopSongs] = useState<Track[]>([])
   const [newReleases, setNewReleases] = useState<SimplifiedAlbum[]>([])
@@ -44,21 +41,12 @@ export default function Page() {
 
   return (
     <div>
-      <button
-        onClick={() => setIsModalOpen(true)}
-        className="mt-4 bg-[var(--color-primary)] text-white px-4 py-2 rounded"
-      >
-        Open Fullscreen Player
-      </button>
-
       <MainElement title="Recently Played" />
       {loading ? <Spinner /> : <TrackGrid tracks={topSongs} />}
       <MainElement title="Your Artists" />
       {loading ? <Spinner /> : <ArtistGrid artists={topArtists} />}
       <MainElement title="New Releases" />
       {loading ? <Spinner /> : <AlbumGrid albums={newReleases} />}
-      <FullScreenPlayer isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
-      {!isModalOpen && <MiniPlayer onOpenFullScreen={() => setIsModalOpen(true)} />}
     </div>
   )
 }
